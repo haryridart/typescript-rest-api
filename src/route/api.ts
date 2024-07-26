8843dc866f2e6d74b322c63e58b8672a93cdcab9
Hary Ridart <hary.teguh@ui.ac.id>
2024-07-26 15:30:32 +0700

    logout API

--- src/route/api.ts
+++ src/route/api.ts
@@ -6,5 +6,6 @@ export const apiRouter = express.Router();
 apiRouter.use(authMiddleware);
 
 // User API
-apiRouter.get("/api/users/current", UserController.get)
-apiRouter.patch("/api/users/current", UserController.update)
+apiRouter.get("/api/users/current", UserController.get);
+apiRouter.patch("/api/users/current", UserController.update);
+apiRouter.delete("/api/users/current", UserController.logout);
